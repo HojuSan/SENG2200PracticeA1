678efce7a05ff33e1ed1498a95eca7c49989f399
HojuSan <juyong.kim@uon.edu.au>
2018-12-11 00:28:43 +1100

    got cygwin to work

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+
+/*
+!!!!!!!!!!!!!!!!!!
+including the .cpp at the end of the header never do unless it is template???
+also to use the make file type in "make" not g++ -o that is calling the compiler directly
+hense i got marked down for it. which is quite fair*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{		
+	cout << "This shit works" << endl;
+	return 0;
+}
